d1e5d57f8de99c95a9fce1c50fba3d93a3537f4d
Nicolò Pasini <nicolo.pasini@igenius.ai>
2021-03-22 09:53:19 +0100

    Added extensions and star gazer cell

---
+++ gazers/Utilities/Extensions/UITableViewCell+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  UITableViewCell+Extensions.swift
+//  gazers
+//
+//  Created by Nicolò Pasini on 21/03/21.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    class var identifier: String {
+        return String(describing: self)
+    }
+}
